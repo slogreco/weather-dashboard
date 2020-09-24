ee4d2bcb1a72f0a0855431e702e124c51b4d60c7
Slogreco <shoshwright@hotmail.com>
2020-09-23 20:58:39 -0700

    tried to get the 5 day forecast to work with not luck

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let APIKey = "785495188f0e1c90cfaa9e54882e6e76";
-let cityState = "phoenix";
+let cityState = "Tucson";
 // $("input").val();
 
 let queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-    "q=" + cityState +"&appid=" + APIKey;
+    "q=" + cityState + "&appid=" + APIKey;
 
 
 $.ajax({
@@ -15,25 +15,53 @@ $.ajax({
         console.log(response);
 
         $(".citySelected").text(response.name)
-        // (moment().format('LT'));
+        // moment().format('LT');
 
         let tempF = (response.main.temp - 273.15) * 1.80 + 32;
-        $("#temperature").text("Temperature: " + tempF.toFixed(1) +" ℉");
+        $("#temperature").text("Temperature: " + tempF.toFixed(1) + " ℉");
         $("#humidity").text("Humidity: " + response.main.humidity + " %");
         $("#wind").text("Wind Speed: " + response.wind.speed + " MPH");
 
         let lat = response.coord.lat
         let lon = response.coord.lon
-    
-            $.ajax({
+
+        $.ajax({
             url: "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey,
             method: "GET"
         })
-            .then(function (response) {    
-                console.log(response);
-            $("#uvIndex").text("UV Index: " + response.value)
+            .then(function (UVResponse) {
+                console.log(UVResponse);
+                $("#uvIndex").text("UV Index: " + UVResponse.value)
             })
-        
     })
 
-    
+
+function getForecast(cityState) {
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/forecast?q=" + cityState + "&appid=" + APIKey,
+        method: "GET"
+    })
+        .then(function (forecastResponse) {
+            for (i = 0; i < forecastResponse.list.length; i++) {
+                if (forecastResponse.list[i].dt_text.indexOf("15:00:00") !== -1) {
+                    let newCard = $("<div>").attr("class", "card text-white bg-primary");
+                    $(".forecastEl").append(newCard);
+
+                    let cardHead = $("<div>").attr("class", "card-header").text(moment(forecastResponse.list[i].dt, "X").format("MMM Do"));
+                    newCard.append(cardHead);
+
+                    let cardImg = $("<img>").attr("class", "card-img-top").attr("src", "https://openweathermap.org/img/wn/" + forecastResponse.list[i].weather[0].icon + "@2x.png");
+                    newCard.append(cardImg);
+
+                    let bodyDiv = $("<div>").attr("class", "card-body");
+                    newCard.append(bodyDiv);
+
+                    bodyDiv.append($("<p>").attr("class", "card-text").html("Temp: " + forecastResponse.list[i].main.temp + " &#8457;"));
+                    bodyDiv.append($("<p>").attr("class", "card-text").text("Humidity: " + forecastResponse.list[i].main.humidity + "%"));
+                }
+            }
+        })
+}
+
+
+
